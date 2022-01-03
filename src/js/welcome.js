0dd6e5c4848b155bf563c0efffc5a93e9b0622ac
Artistlk <96751228+artistlk@users.noreply.github.com>
2022-01-03 22:57:01 +0100

    test

---
+++ src/js/welcome.js
@@ -0,0 +1,14 @@
+//Language Selector
+
+//Call all the languages
+
+const lang_es = document.getElementById('es-es');
+const lang_en = document.getElementById('en-en');
+
+lang_en.addEventListener('click',()=>{
+    alert('Nigga!');
+});
+
+lang_es.addEventListener('click',()=>{
+    alert('White!');
+});
