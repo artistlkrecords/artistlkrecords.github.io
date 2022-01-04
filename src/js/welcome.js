e035690b74f1421f83ebe892168ab11079fd8722
Artistlk <96751228+artistlk@users.noreply.github.com>
2022-01-04 23:34:26 +0100

    Some debugin on selector

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -2,9 +2,16 @@
 
 //Call all the languages
 
+const lang_def = document.getElementById('lang-sel');
 const lang_es = document.getElementById('es-es');
 const lang_en = document.getElementById('en-en');
 const sel_btn = document.getElementById('lang-sel-btn');
+
+
+document.body.onload = function() {
+    lang_def.value = 'def-sel';
+    alert("Options Reseted!");
+}
 
 lang_en.addEventListener('click',()=>{
     sel_btn.innerText = "Enter";
